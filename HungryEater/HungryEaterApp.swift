2ce79ac9e8e06a361ff48852f0343e4a645c2c73
Ila <iferrier2025@dominicanacademy.org>
2023-08-01 13:26:29 -0400

    Initial Commit

---
+++ HungryEater/HungryEaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungryEaterApp.swift
+//  HungryEater
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HungryEaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
